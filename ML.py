3e380fb747a28550e915aef8fb45ad7ac1d69d0b
Ilya Prokin <isprokin@gmail.com>
2016-10-10 01:28:45 +0200

    fixes

--- ML.py
+++ ML.py
@@ -1,8 +1,8 @@
 import feedparser
 import os
-#from collections import OrderedDict
+from collections import OrderedDict
 
-from doglib import feed_to_md_file, process_an_entry
+from doglib import feed_to_md_file, process_an_entry, file_to_str, rating_from_md_files_in_folder
 
 # ML
 
@@ -19,11 +19,14 @@ print(d['feed']['title'])
 
 # load simplified entries
 entries = list(map(process_an_entry, d['entries']))
+# get ids
+ids = [e['id'] for e in entries]
 
 # throw away timestamps and ids
 for e in entries:
     e.pop('timestamp')
     e.pop('id')
+
 
 # title and text are combined
 txts = list(map(
@@ -32,6 +35,33 @@ txts = list(map(
                 entries
                )
            )
-#labels = # load from some where
+
+
+labels = rating_from_md_files_in_folder(os.path.join(path, 'md')) # load from some where
+
+
+d_idstxts = dict(zip(ids, txts))
+
+stopwords = set(file_to_str('./stopwords.txt').split('\n'))
+
+def create_empty_word_count_table():
+    return {
+        0:OrderedDict(),
+        1:OrderedDict()
+    }
+
+# init tables
+word_counts = create_empty_word_count_table()
+sum_dict = {0:0, 1:0}
+
+for k in d_idstxts.keys():
+    if k in labels.keys():
+        for word in d_idstxts[k]:
+            if word not in stopwords:
+                if word in word_counts[labels[k]].keys():
+                    word_counts[labels[k]][word] += 1
+                    sum_dict[labels[k]] += 1
+                else:
+                    word_counts[labels[k]][word] = 0
 
 
--- doglib.py
+++ doglib.py
@@ -1,7 +1,13 @@
 import re
 import time
+import os
 
-# preprocess
+
+def file_to_str(filepath):
+    with open(filepath, 'r') as f:
+        s = f.read()
+    return s
+
 
 def notags(s):
     return re.sub('<[^>]+>', '', s)
@@ -21,7 +27,8 @@ def to_readable_md(e):
         s += '# ' + e['title'].strip() + '\n\n'
         if 'authors' in e.keys():
             s += '## ' + ', '.join(list(map(lambda x: x['name'], e['authors']))) + '\n\n'
-        s += notags(e['content'][0]['value']).replace('\n', ' ').strip() + '\n\n'
+        if 'content' in e.keys():
+            s += notags(e['content'][0]['value']).replace('\n', ' ').strip() + '\n\n'
         s += e['id']
         s = re.sub(' +', ' ', s)
         return s
@@ -30,12 +37,59 @@ def to_readable_md(e):
 
 
 def feed_to_md_file(d, fpath):
-    mds = list(filter(lambda s: s != '', map(to_readable_md, d['entries'])))
+    #mds = [s for s in map(to_readable_md, d['entries']) if s != '']
+    mds = filter(lambda s: s != '', map(to_readable_md, d['entries']))
     md = '\n\n-------------\n\n'.join(mds)
-    md = '# ' + d['feed']['title'] + '\n\n' + md.replace('#', '##')
+    md = '# ' + d['feed']['title'] + '\n\n' + re.sub('([#]+)', r'\1#', md)
     #print(md)
     with open(fpath, 'w') as f:
         f.write(md)
+
+
+# DELETE THIS FUNCTION
+def ids_to_file(path, files, outfullpath):
+    # make file with ids
+
+    fullpaths = map(lambda f: os.path.join(path, f), infiles)
+    s = ''
+    for fullpath in fullpaths:
+        d = feedparser.parse(fullpath)
+        ids = [e['id'] for e in d['entries']]
+        s += ' \n'.join(ids)
+        s += '\n'
+
+    with open(outfullpath, 'w') as f:
+        f.write(s)
+
+
+def rating_from_md_file(mdfile):
+    """
+    Reads user-entered rating from mdfile.
+    To rate news item, edit mdfile directly: add rating after id separated by
+    one space character.
+    E.g.:
+    Before edit:
+        https://arstechnica.com/?p=972521
+    After edit:
+        https://arstechnica.com/?p=972521 0
+    """
+    mds = file_to_str(mdfile).split('\n\n-------------\n\n')
+    id_rating = map(
+        lambda md:
+            (lambda x:
+                (x[0], None) if len(x)==1 else (x[0], int(x[1]))
+            )(md.split('\n')[-1].split(' '))
+        ,
+        mds
+       )
+    return dict(id_rating)
+
+
+def rating_from_md_files_in_folder(mdfolder):
+    mdfiles = list(filter(lambda s: s[0] != '.' and s[-3:] == '.md', os.listdir(mdfolder)))
+    fullpaths = map(lambda x: os.path.join(mdfolder, x), mdfiles)
+    items_combined = sum(map(lambda x: list(rating_from_md_file(x).items()), fullpaths), [])
+    return dict(filter(lambda x: x[1] != None, items_combined))
 
 
 def process_an_entry(e):
@@ -45,16 +99,16 @@ def process_an_entry(e):
             map(
                 lambda s:
                     nopunctuation(
-                        notags(s).replace('\n', '')
+                        notags(s).replace('\n', '').replace('\xa0', ' ')
                     ).strip().lower().split(' ')
                 ,
                 [
                     e['title'],
-                    e['content'][0]['value']
+                    e['content'][0]['value'] if 'content' in e.keys() else ''
                 ]
             )
          )
-    l += e['id']
+    l += [e['id']]
     # give names to items
     l = dict(
             zip(
