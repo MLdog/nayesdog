9f27c87770e98c57b1183cf12418842537b1438c
Ilya Prokin <isprokin@gmail.com>
2016-11-20 20:01:24 +0100

    better version in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
+version = '0.1.3'
 setup(
     name = 'nayesdog',
     packages = ['nayesdog'],
-    version = '0.1.3',
+    version = version,
     description = 'RSS reader with Naive Bayes powered recommendations',
     author = 'Ilya Prokin, Sergio Peignier',
     author_email = 'isprokin@gmail.com, sergio.peignier.zapata@gmail.com',
     url = 'http://github.com/MLdog/nayesdog',
-    download_url ='http://github.com/MLdog/nayesdog/tarball/0.1',
+    download_url = 'https://github.com/MLdog/nayesdog/archive/v{}.tar.gz'.format(version),
     keywords = ['RSS reader', 'Naive Bayes', 'recommendations'],
     classifiers = [],
-    package_data = {'nayesdog':['icons/*','css.css','stopwords.txt']},
+    #package_data = {'nayesdog':['icons/*','css.css','stopwords.txt']},
     include_package_data=True,
     entry_points = """
       [console_scripts]
